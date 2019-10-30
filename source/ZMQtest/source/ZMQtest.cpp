57c7e5d61384af942eb7415c0b579c0743923f95
Lawrence Qin <qinwenbo1991@163.com>
2019-10-30 17:58:49 +0800

    init version

---
+++ source/ZMQtest/source/ZMQtest.cpp
@@ -0,0 +1,17 @@
+#include "ZMQtest.h"
+
+void start_send()
+{
+    zsock_t *pub_ = pub_ = zsock_new_pub("127.0.0.1");
+}
+
+void start_recv()
+{
+    zsock_t *pub_ = pub_ = zsock_new_pub("127.0.0.1");
+}
+
+int main()
+{
+
+}
+
